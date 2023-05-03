270bbc5d2a58d86608f05eb52b722838707689f8
ryoshindo <udoryo999@gmail.com>
2023-05-03 12:42:15 +0900

    feat: implement virtual node deploy

--- deploy.go
+++ deploy.go
@@ -3,23 +3,58 @@ package spider
 import (
 	"context"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/appmesh"
-	"github.com/aws/aws-sdk-go-v2/service/appmesh/types"
 )
 
-func (s *App) Deploy(ctx context.Context) error {
-	input := &appmesh.CreateVirtualNodeInput{
-		MeshName:        aws.String(appmeshName),     // FIXME
-		VirtualNodeName: aws.String(virtualNodeName), // FIXME
-		Spec: &types.VirtualNodeSpec{
-			Listeners: []types.Listener{},
-		},
+func (s *App) Describe(ctx context.Context) error {
+	if _, err := s.DescribeVirtualNode(ctx); err != nil {
+		return err
 	}
 
-	_, err := s.appmesh.CreateVirtualNode(ctx, input)
+	return nil
+}
+
+func (s *App) DescribeVirtualNode(ctx context.Context) (*appmesh.DescribeVirtualNodeOutput, error) {
+	vn := &describeVirtualNode{s}
+	input, err := vn.Load(s.config.VirtualNodes[0]) // FIXME: Allow for multiple file support
 	if err != nil {
+		return &appmesh.DescribeVirtualNodeOutput{}, err
+	}
+
+	output, err := s.appmesh.DescribeVirtualNode(ctx, input)
+	if err != nil {
+		return &appmesh.DescribeVirtualNodeOutput{}, err
+	}
+
+	return output, nil
+}
+
+func (s *App) Deploy(ctx context.Context) error {
+	if err := s.DeployVirtualNode(ctx); err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func (s *App) DeployVirtualNode(ctx context.Context) error {
+	_, err := s.DescribeVirtualNode(ctx)
+	if err == nil {
+		vn := &updateVirtualNode{s}
+		input, err := vn.Load(s.config.VirtualNodes[0]) // FIXME: Allow for multiple file support
+
+		_, err = s.appmesh.UpdateVirtualNode(ctx, input)
+		if err != nil {
+			return err
+		}
+	} else {
+		vn := &createVirtualNode{s}
+		input, err := vn.Load(s.config.VirtualNodes[0]) // FIXME: Allow for multiple file support
+
+		_, err = s.appmesh.CreateVirtualNode(ctx, input)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
---
+++ virtual_node.go
@@ -0,0 +1,122 @@
+package spider
+
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/aws/aws-sdk-go-v2/service/appmesh"
+)
+
+type VirtualNodeDefinition[T VirtualNodeDefinitionInput] interface {
+	Load(path string) (T, error)
+}
+
+type VirtualNodeDefinitionInput interface {
+	*appmesh.DescribeVirtualNodeInput | *appmesh.CreateVirtualNodeInput | *appmesh.UpdateVirtualNodeInput
+}
+
+type describeVirtualNode struct {
+	*App
+}
+
+func (v *describeVirtualNode) Load(path string) (*appmesh.DescribeVirtualNodeInput, error) {
+	src, err := v.readDefinitionFile(path)
+	if err != nil {
+		return &appmesh.DescribeVirtualNodeInput{}, err
+	}
+
+	c := struct {
+		VirtualNodeDefinition json.RawMessage `json:"virtualNodeDefinition"`
+	}{}
+
+	dec := json.NewDecoder(bytes.NewReader(src))
+	if err := dec.Decode(&c); err != nil {
+		return &appmesh.DescribeVirtualNodeInput{}, err
+	}
+
+	if c.VirtualNodeDefinition != nil {
+		src = c.VirtualNodeDefinition
+	}
+
+	input := appmesh.DescribeVirtualNodeInput{
+		MeshName:  &v.config.Mesh.Name,
+		MeshOwner: &v.config.Mesh.Owner,
+	}
+	if err := v.UnmarshalJsonForStruct(src, &input, path); err != nil {
+		return &appmesh.DescribeVirtualNodeInput{}, err
+	}
+
+	return &input, nil
+}
+
+type createVirtualNode struct {
+	*App
+}
+
+func (v *createVirtualNode) Load(path string) (*appmesh.CreateVirtualNodeInput, error) {
+	src, err := v.readDefinitionFile(path)
+	if err != nil {
+		return &appmesh.CreateVirtualNodeInput{}, err
+	}
+
+	c := struct {
+		VirtualNodeDefinition json.RawMessage `json:"virtualNodeDefinition"`
+	}{}
+
+	dec := json.NewDecoder(bytes.NewReader(src))
+	if err := dec.Decode(&c); err != nil {
+		return &appmesh.CreateVirtualNodeInput{}, err
+	}
+
+	if c.VirtualNodeDefinition != nil {
+		src = c.VirtualNodeDefinition
+	}
+
+	input := appmesh.CreateVirtualNodeInput{
+		MeshName:  &v.config.Mesh.Name,
+		MeshOwner: &v.config.Mesh.Owner,
+	}
+	if err := v.UnmarshalJsonForStruct(src, &input, path); err != nil {
+		return &appmesh.CreateVirtualNodeInput{}, err
+	}
+
+	if len(input.Tags) == 0 {
+		input.Tags = nil
+	}
+
+	return &input, nil
+}
+
+type updateVirtualNode struct {
+	*App
+}
+
+func (v *updateVirtualNode) Load(path string) (*appmesh.UpdateVirtualNodeInput, error) {
+	src, err := v.readDefinitionFile(path)
+	if err != nil {
+		return &appmesh.UpdateVirtualNodeInput{}, err
+	}
+
+	c := struct {
+		VirtualNodeDefinition json.RawMessage `json:"virtualNodeDefinition"`
+	}{}
+
+	dec := json.NewDecoder(bytes.NewReader(src))
+	if err := dec.Decode(&c); err != nil {
+		return &appmesh.UpdateVirtualNodeInput{}, err
+	}
+
+	if c.VirtualNodeDefinition != nil {
+		src = c.VirtualNodeDefinition
+	}
+
+	input := appmesh.UpdateVirtualNodeInput{
+		MeshName:  &v.config.Mesh.Name,
+		MeshOwner: &v.config.Mesh.Owner,
+	}
+	if err := v.UnmarshalJsonForStruct(src, &input, path); err != nil {
+		return &appmesh.UpdateVirtualNodeInput{}, err
+	}
+
+	return &input, nil
+}
