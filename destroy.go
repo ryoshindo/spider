457ecfc8a4124ef8d613f504fcf2191d63c49087
ryoshindo <udoryo999@gmail.com>
2023-05-04 10:09:22 +0900

    fix: add if else statement to destroy function to determine if the resource exists or not

--- destroy.go
+++ destroy.go
@@ -32,15 +32,20 @@ func (s *App) Destroy(ctx context.Context) error {
 
 func (s *App) DestroyVirtualNode(ctx context.Context) error {
 	for _, virtualNode := range s.config.VirtualNodes {
-		vn := &DeleteVirtualNode{s}
-		input, err := vn.Load(virtualNode)
-		if err != nil {
-			return err
-		}
+		output, _ := s.DescribeVirtualNode(ctx, virtualNode)
+		if output.VirtualNode == nil {
+			continue
+		} else {
+			vn := &DeleteVirtualNode{s}
+			input, err := vn.Load(virtualNode)
+			if err != nil {
+				return err
+			}
 
-		_, err = s.appmesh.DeleteVirtualNode(ctx, input)
-		if err != nil {
-			return err
+			_, err = s.appmesh.DeleteVirtualNode(ctx, input)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
@@ -49,15 +54,20 @@ func (s *App) DestroyVirtualNode(ctx context.Context) error {
 
 func (s *App) DestroyVirtualRouter(ctx context.Context) error {
 	for _, virtualRouter := range s.config.VirtualRouters {
-		vr := &DeleteVirtualRouter{s}
-		input, err := vr.Load(virtualRouter.Path)
-		if err != nil {
-			return err
-		}
+		output, _ := s.DescribeVirtualRouter(ctx, virtualRouter.Path)
+		if output.VirtualRouter == nil {
+			continue
+		} else {
+			vr := &DeleteVirtualRouter{s}
+			input, err := vr.Load(virtualRouter.Path)
+			if err != nil {
+				return err
+			}
 
-		_, err = s.appmesh.DeleteVirtualRouter(ctx, input)
-		if err != nil {
-			return err
+			_, err = s.appmesh.DeleteVirtualRouter(ctx, input)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
@@ -68,15 +78,19 @@ func (s *App) DestroyRoute(ctx context.Context) error {
 	for _, virtualRouter := range s.config.VirtualRouters {
 		for _, route := range virtualRouter.Routes {
 			output, _ := s.DescribeRoute(ctx, route, virtualRouter.Path)
-			r := &DeleteRoute{s}
-			input, err := r.Load(route, *output.Route.VirtualRouterName)
-			if err != nil {
-				return err
-			}
+			if output.Route == nil {
+				continue
+			} else {
+				r := &DeleteRoute{s}
+				input, err := r.Load(route, *output.Route.VirtualRouterName)
+				if err != nil {
+					return err
+				}
 
-			_, err = s.appmesh.DeleteRoute(ctx, input)
-			if err != nil {
-				return err
+				_, err = s.appmesh.DeleteRoute(ctx, input)
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
@@ -86,15 +100,20 @@ func (s *App) DestroyRoute(ctx context.Context) error {
 
 func (s *App) DestroyVirtualService(ctx context.Context) error {
 	for _, virtualService := range s.config.VirtualServices {
-		vs := &DeleteVirtualService{s}
-		input, err := vs.Load(virtualService)
-		if err != nil {
-			return err
-		}
+		output, _ := s.DescribeVirtualService(ctx, virtualService)
+		if output.VirtualService == nil {
+			continue
+		} else {
+			vs := &DeleteVirtualService{s}
+			input, err := vs.Load(virtualService)
+			if err != nil {
+				return err
+			}
 
-		_, err = s.appmesh.DeleteVirtualService(ctx, input)
-		if err != nil {
-			return err
+			_, err = s.appmesh.DeleteVirtualService(ctx, input)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
@@ -103,15 +122,20 @@ func (s *App) DestroyVirtualService(ctx context.Context) error {
 
 func (s *App) DestroyVirtualGateway(ctx context.Context) error {
 	for _, virtualGateway := range s.config.VirtualGateways {
-		vg := &DeleteVirtualGateway{s}
-		input, err := vg.Load(virtualGateway.Path)
-		if err != nil {
-			return err
-		}
+		output, _ := s.DescribeVirtualGateway(ctx, virtualGateway.Path)
+		if output.VirtualGateway == nil {
+			continue
+		} else {
+			vg := &DeleteVirtualGateway{s}
+			input, err := vg.Load(virtualGateway.Path)
+			if err != nil {
+				return err
+			}
 
-		_, err = s.appmesh.DeleteVirtualGateway(ctx, input)
-		if err != nil {
-			return err
+			_, err = s.appmesh.DeleteVirtualGateway(ctx, input)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
@@ -122,15 +146,19 @@ func (s *App) DestroyGatewayRoute(ctx context.Context) error {
 	for _, virtualGateway := range s.config.VirtualGateways {
 		for _, gatewayRoute := range virtualGateway.GatewayRoutes {
 			output, _ := s.DescribeGatewayRoute(ctx, gatewayRoute, virtualGateway.Path)
-			gr := &DeleteGatewayRoute{s}
-			input, err := gr.Load(gatewayRoute, *output.GatewayRoute.VirtualGatewayName)
-			if err != nil {
-				return err
-			}
+			if output.GatewayRoute == nil {
+				continue
+			} else {
+				gr := &DeleteGatewayRoute{s}
+				input, err := gr.Load(gatewayRoute, *output.GatewayRoute.VirtualGatewayName)
+				if err != nil {
+					return err
+				}
 
-			_, err = s.appmesh.DeleteGatewayRoute(ctx, input)
-			if err != nil {
-				return err
+				_, err = s.appmesh.DeleteGatewayRoute(ctx, input)
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
